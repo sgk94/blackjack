37494bb3058392c05a132165862db1ec86ca09c6
Shawn Dev <kimsha004@gmail.com>
2021-01-08 02:16:19 -0800

    work

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-/*---- Constants ----*/
+/*---- Class ----*/
+
+class Deck {
+    constructer() {
+        this.deck = [];
+
+        const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+        const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+        
+    }
+}
 
 /*---- App's State (variable) ----*/
 
+
 /*---- Cached Element References ----*/
 
+
 /*---- Event Listeners ----*/
+
 
 /*---- Functions ----*/
 
