f99af5c3a63b32132a113301a9e9f9f86858bcfa
Shawn Dev <kimsha004@gmail.com>
2021-01-08 02:50:13 -0800

    stuff added

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ class Deck {
 
         const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
         const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-        
+
+        for (let suit in suits) {
+            for (let value in values) {
+                this.deck.push(`${values[value]} of ${suits[suit]}`);
+            }
+        }
     }
 }
 
+const deck1 = new Deck();
+console.log(deck1);
 /*---- App's State (variable) ----*/
 
 
---
+++ test.js
@@ -0,0 +1,1 @@
+import '../blackjack/app.js'
