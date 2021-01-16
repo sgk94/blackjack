57340911368b162ab0e09392e09d6dfb4b318890
Shawn Dev <kimsha004@gmail.com>
2021-01-16 07:52:36 -0800

    added logic for creating a deck and shuffling the deck

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 // Deck
-var deck = [];
+var deck = new Array();
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
-const card = "";
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const shoot = new Object();
 
 // Create the deck
-function createDeck (suits, values, deck, card) {
+function createDeck (suits, values, deck) {
+    // Iterate through suits array
     for (i=0; i < suits.length; i++) {
-        // console.log("suits"+suits[i]);
+    // Iterate through values array
         for (j= 0; j < values.length; j++) {
-            // console.log(values[j] + "of" + suits[i])
-            card = values[j] + " " + "of" + " " + suits[i]
-            console.log("this is my card"+card);
+            var weight = parseInt(values[j]);
+            if (values[j] === "J" || values[j] === "Q" || values[j] === "K") {
+                weight = 10;
+            };
+            if (values[j] === "A") {
+                weight = 11;
+            };
+            var card = { Value: values[j], Suit: suits[i], Weight: weight };
             deck.push(card);
-            // console.log("deck in the making "+deck);
         };
     };
-
     console.log(deck);
-    // console.log(suits + values);
 };
 
-createDeck(suits, values, deck, card);
+function shuffle (deck) {
+    for (var i = 0; i < 100; i++) {
+        var l1 = Math.floor((Math.random() * deck.length));
+        console.log(l1);
+        var l2 = Math.floor((Math.random() * deck.length));
+        console.log(l2);
+        var tmp = deck[l1];
+
+        console.log(deck[l1] = deck[l2]);
+        console.log(deck[l2] = tmp);
+    };
+    console.log(deck);
+};
+
+function init (suits, values, deck) {
+    createDeck(suits, values, deck);
+    shuffle(deck);
+};
+
+init(suits, values, deck);
+// Create the shoot
+// function createShoot (deck, suits, values, card, shoot) {
+//     for (i = 0; i = 6; i++) {
+//         createDeck(suits, values, deck, card);
+//         shoot
+//     };
+//     console.log(shoot);
+// };
+// Functions
+// createShoot(deck, suits, values, card, shoot);
+
 
 
