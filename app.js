e63136f66498a54e0132835a6fab6b9c129c0fc1
Shawn Dev <kimsha004@gmail.com>
2021-01-15 18:38:04 -0800

    trying something different

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
+//deck
+var deck = [];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
 
-/*class Deck {
-    constructer() {
-        this.deck = [];
-
-        const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-        const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-
-        for (let suit in suits) {
-            for (let value in values) {
-                this.deck.push(`${values[value]} of ${suits[suit]}`);
-            }
-        }
-    }
-};
-
-const deck1 = new Deck();
-console.log(deck1);*/
-
-var msg = "hello";
-
-console.log(msg);
 
 
 
